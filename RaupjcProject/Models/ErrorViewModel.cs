9ee8b7b0033f861a6328a9e4e14a734964556851
nick02090 <32841832+nick02090@users.noreply.github.com>
2018-02-12 12:31:28 +0100

    Add project files.

---
+++ RaupjcProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RaupjcProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
